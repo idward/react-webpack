100162aa2116c3631ca6774bc96927f0fe41c932
idwardluo <apple@idwardluo.local>
2017-06-25 19:15:05 +0800

    update react-webpack

--- src/index.js
+++ src/index.js
@@ -2,29 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/main.css';
 
+// function Square(props){
+//     return (
+//         <button className="square" onClick={props.click}>{props.value}</button>
+//     )
+// }
 
 class Square extends React.Component {
-    constructor(){
-        super();
-        this.state = {value:null};
-        console.log(this.state);
-    }
     render() {
         return (
-            <button className="square" onClick={() => this.setState({value:'X'})}>
-                {this.state.value ? this.state.value : this.props.value}
+            <button className="square" onClick={this.props.click.bind(this)}>
+                {this.props.value}
             </button>
         );
     };
 }
 
 class Board extends React.Component {
-    renderSquare(i){
-        return <Square value={i} />;
+    constructor() {
+        super();
+        this.state = {
+            squares: new Array(9).fill(null),
+            xIsNext: true
+        };
     }
 
-    render(){
-        const status = 'Next player: X';
+    handleClick(i) {
+        //copy new array object (without mutate)
+        const newSquares = this.state.squares.slice();
+        newSquares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: newSquares,
+            xIsNext: !this.state.xIsNext
+        });
+    }
+
+    renderSquare(i) {
+        return <Square value={this.state.squares[i] ? this.state.squares[i] : i} click={()=>this.handleClick(i)}/>;
+    }
+
+    render() {
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
@@ -65,6 +83,29 @@ class Game extends React.Component {
     }
 }
 
+//计算规则
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+        const [a,b,c] = lines[i];
+        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+            return squares[a];
+        }
+    }
+
+    return null;
+}
+
 const app = document.getElementById('app');
 
-ReactDOM.render(<Game/>,app);
+ReactDOM.render(<Game/>, app);
